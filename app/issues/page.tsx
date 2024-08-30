570426fcfc8f475ff7e5dc532ae5a3f9cf2b2d9f
Sanskar Chirania <sanskar125112@gmail.com>
2024-08-30 12:02:16 +0530

    Set up redix ui

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,7 +1,12 @@
 import React from "react";
+import { Button } from "@radix-ui/themes";
 
 function issues() {
-  return <div>issues</div>;
+  return (
+    <div>
+      <Button>New Issue</Button>
+    </div>
+  );
 }
 
 export default issues;
