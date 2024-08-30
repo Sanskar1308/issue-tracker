e7b24bd6a388037d212ba70984b479f38bb2116c
Sanskar Chirania <sanskar125112@gmail.com>
2024-08-30 16:36:56 +0530

    Created spinner

---
+++ app/components/Spinner.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Spinner() {
+  return (
+    <div
+      className="animate-spin inline-block size-4 border-[3px] border-current border-t-transparent text-white rounded-full dark:text-white"
+      role="status"
+      aria-label="loading"
+    >
+      <span className="sr-only">Loading...</span>
+    </div>
+  );
+}
+
+export default Spinner;
--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -10,6 +10,8 @@ import { useState } from "react";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { createIssueSchema } from "@/app/validationSchema";
 import { z } from "zod";
+import ErrorMessage from "@/app/components/ErrorMessage";
+import Spinner from "@/app/components/Spinner";
 
 type IssueForm = z.infer<typeof createIssueSchema>;
 
@@ -24,6 +26,7 @@ function NewIssuePage() {
     resolver: zodResolver(createIssueSchema),
   });
   const [error, setError] = useState("");
+  const [issubmitting, setIssubmitting] = useState(false);
   return (
     <div className="max-w-xl">
       {error && (
@@ -35,9 +38,11 @@ function NewIssuePage() {
         className="space-y-3"
         onSubmit={handleSubmit(async (data) => {
           try {
+            setIssubmitting(true);
             await axios.post("/api/issues", data);
             router.push("/issues");
           } catch (error) {
+            setIssubmitting(false);
             setError("Unexpected error occured!!!");
           }
         })}
@@ -46,11 +51,7 @@ function NewIssuePage() {
           placeholder="Title"
           {...register("title")}
         ></TextField.Root>
-        {errors.title && (
-          <Text color="red" as="p">
-            {errors.title.message}
-          </Text>
-        )}
+        <ErrorMessage>{errors.title?.message}</ErrorMessage>
         <Controller
           name="description"
           control={control}
@@ -58,12 +59,10 @@ function NewIssuePage() {
             <SimpleMDE placeholder="Description" {...field} />
           )}
         />
-        {errors.description && (
-          <Text color="red" as="p">
-            {errors.description.message}
-          </Text>
-        )}
-        <Button>Submit New Issue</Button>
+        <ErrorMessage>{errors.description?.message}</ErrorMessage>
+        <Button disabled={issubmitting}>
+          Submit New Issue {issubmitting && <Spinner />}
+        </Button>
       </form>
     </div>
   );
