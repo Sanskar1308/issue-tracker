a78c7653cbf10ad139655b99d0a803283b91becc
Sanskar Chirania <sanskar125112@gmail.com>
2024-09-03 11:33:31 +0530

    Show a confirmation dialogue

--- app/issues/[id]/issueDelete.tsx
+++ app/issues/[id]/issueDelete.tsx
@@ -1,8 +1,33 @@
-import { Button } from "@radix-ui/themes";
+"use client";
+
+import { AlertDialog, Button, Flex } from "@radix-ui/themes";
 import React from "react";
 
 const IssueDelete = async ({ issueId }: { issueId: number }) => {
-  return <Button color="red">Delete issue</Button>;
+  return (
+    <AlertDialog.Root>
+      <AlertDialog.Trigger>
+        <Button color="red">Delete</Button>
+      </AlertDialog.Trigger>
+      <AlertDialog.Content>
+        <AlertDialog.Title>Confirm Delete</AlertDialog.Title>
+        <AlertDialog.Description>
+          Are you sure you want to delete this issue? This action can't be
+          undone.
+        </AlertDialog.Description>
+        <Flex mt="5" gap="2">
+          <AlertDialog.Cancel>
+            <Button variant="soft" color="gray">
+              Cancel
+            </Button>
+          </AlertDialog.Cancel>
+          <AlertDialog.Action>
+            <Button color="red">Delete Issue</Button>
+          </AlertDialog.Action>
+        </Flex>
+      </AlertDialog.Content>
+    </AlertDialog.Root>
+  );
 };
 
 export default IssueDelete;
