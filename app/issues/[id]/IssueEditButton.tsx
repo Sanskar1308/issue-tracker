278917c2eadd54976327827aad0993eaefb85e34
Sanskar Chirania <sanskar125112@gmail.com>
2024-09-03 11:47:21 +0530

    Completed delete functionality in both issue detail and edit page

--- app/issues/[id]/IssueEditButton.tsx
+++ app/issues/[id]/IssueEditButton.tsx
@@ -1,13 +1,12 @@
 import { Button } from "@radix-ui/themes";
 import Link from "next/link";
-import React from "react";
 import { FaRegEdit } from "react-icons/fa";
 
 const IssueEditButton = ({ issueId }: { issueId: number }) => {
   return (
     <Button>
       <FaRegEdit />
-      <Link href={`/issues/${issueId}/edit`}>Edit Button</Link>
+      <Link href={`/issues/${issueId}/edit`}>Edit</Link>
     </Button>
   );
 };
--- app/issues/[id]/issueDelete.tsx
+++ app/issues/[id]/issueDelete.tsx
@@ -1,13 +1,32 @@
 "use client";
 
-import { AlertDialog, Button, Flex } from "@radix-ui/themes";
-import React from "react";
+import { AlertDialog, Button, Flex, Spinner } from "@radix-ui/themes";
+import axios from "axios";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+import { MdOutlineDeleteOutline } from "react-icons/md";
 
 const IssueDelete = async ({ issueId }: { issueId: number }) => {
+  const [issubmittingDelete, setIssubmittingDelete] = useState(false);
+  const router = useRouter();
+
+  const handleDelete = async () => {
+    try {
+      setIssubmittingDelete(true);
+      await axios.delete("/api/issues/" + issueId);
+      router.push("/issues");
+      router.refresh();
+    } catch (error) {
+      setIssubmittingDelete(false);
+    }
+  };
   return (
     <AlertDialog.Root>
       <AlertDialog.Trigger>
-        <Button color="red">Delete</Button>
+        <Button color="red">
+          <MdOutlineDeleteOutline />
+          Delete
+        </Button>
       </AlertDialog.Trigger>
       <AlertDialog.Content>
         <AlertDialog.Title>Confirm Delete</AlertDialog.Title>
@@ -22,7 +41,13 @@ const IssueDelete = async ({ issueId }: { issueId: number }) => {
             </Button>
           </AlertDialog.Cancel>
           <AlertDialog.Action>
-            <Button color="red">Delete Issue</Button>
+            <Button
+              color="red"
+              onClick={handleDelete}
+              disabled={issubmittingDelete}
+            >
+              Delete Issue {issubmittingDelete && <Spinner />}
+            </Button>
           </AlertDialog.Action>
         </Flex>
       </AlertDialog.Content>
