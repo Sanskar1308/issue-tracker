409d66552a0f957b050629feae6fcaedc8addccb
Sanskar Chirania <sanskar125112@gmail.com>
2025-06-16 10:44:06 +0530

    added click event whole button rather than text

--- app/issues/[id]/IssueEditButton.tsx
+++ app/issues/[id]/IssueEditButton.tsx
@@ -2,13 +2,13 @@ import { Button } from "@radix-ui/themes";
 import Link from "next/link";
 import { FaRegEdit } from "react-icons/fa";
 
-const IssueEditButton = ({ issueId }: { issueId: string }) => {
-  return (
-    <Button>
+const IssueEditButton = ({ issueId }: { issueId: string }) => (
+  <Button asChild>
+    <Link href={`/issues/edit/${issueId}`}>
       <FaRegEdit />
-      <Link href={`/issues/edit/${issueId}`}>Edit</Link>
-    </Button>
-  );
-};
+      Edit
+    </Link>
+  </Button>
+);
 
 export default IssueEditButton;
